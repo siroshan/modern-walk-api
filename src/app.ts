d11d161b908f1af7468bdea88a8a2308af38b9da
shiroshan <siroshang@gmail.com>
2023-07-31 15:13:51 +0530

    feat: Install swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, Express } from 'express';
+import swaggerUi from "swagger-ui-express";
 import userRouter from './routes/user.route';
 import productRouter from './routes/product.route';
 import categoryRouter from './routes/category.route';
@@ -7,12 +8,23 @@ const app: Express = express();
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Modernwalk API');
-});
-
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/categories', categoryRouter);
+
+app.use(express.static('dist'));
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: '/swagger.json',
+    },
+  })
+);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Modernwalk API');
+});
 
 export default app;
